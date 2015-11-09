4254f996ab24b8e0e44dd34e04d818938276b21b
Greg Block <gblock0@gmail.com>
2015-11-09 14:52:41 -0600

    Added g clone to add tpm on install

--- install.sh
+++ install.sh
@@ -26,6 +26,9 @@ if [ "$(uname)" == "Darwin" ]; then
 		echo "install plug.vim"
 		curl -# --create-dirs -o ~/.dotfiles/vim/vim.symlink/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 		curl -# --create-dirs -o ~/.dotfiles/config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+		echo "Add tmux plugin manager"
+		git clone https://github.com/tmux-plugins/tpm tmux/tmux.symlink/plugins/tpm
 fi
 
 
