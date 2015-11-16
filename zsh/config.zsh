2e31907482dee8a2e1af5b0466f8c83269b511b1
Greg Block <gblock0@gmail.com>
2015-11-16 08:14:22 -0600

    search command history with whatever has been typed

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -29,6 +29,8 @@ bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
+bindkey "^[[A" history-beginning-search-backward
+bindkey "^[[B" history-beginning-search-forward
 
 fpath=($ZSH/functions $fpath)
 autoload -U $ZSH/functions/*(:t)
