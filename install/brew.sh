860dfa8819397f4ef8199ce18b1c06ebc310c6fb
Greg Block <gblock0@gmail.com>
2020-06-04 18:53:36 -0700

    Cleanup zsh settings and brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -6,21 +6,13 @@ brew update
 brew install the_silver_searcher
 brew install tree
 brew install wget
+brew install ripgrep
 
 # development tools
 brew install git
-brew install ctags
-brew install hub
-brew install ruby
-brew install mongodb --with-openssl
-brew install python
 brew install reattach-to-user-namespace
 brew install tmux
 brew install zsh
-brew install highlight
-brew install nvm
-brew install z
-brew install docker
-brew install docker-machine
 brew install fzf
 brew install romkatv/powerlevel10k/powerlevel10k
+brew install neovim
