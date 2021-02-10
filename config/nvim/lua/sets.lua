a58f6222825abab6ac9988e71c7d1374c550e5b4
Greg Block <gblock0@gmail.com>
2021-02-10 10:34:55 -0800

    Setup folding

--- config/nvim/lua/sets.lua
+++ config/nvim/lua/sets.lua
@@ -3,35 +3,28 @@ opt = utils.opt
 
 vim.g.mapleader = ' '
 
-opt('o', 'termguicolors', true)
-
-opt('o', 'mouse', 'a')
-
 -- Tab control
 opt('b', 'tabstop', 2) -- the visible width of tabs
 opt('b', 'softtabstop', 2) -- edit as if the tabs are 4 characters wide
 opt('b', 'shiftwidth', 2) -- number of spaces to use for indent and unindent
 opt('b', 'expandtab', true) -- insert tabs rather than spaces for <Tab>
-opt('o', 'shiftround', true) -- round indent to a multiple of 'shiftwidth'
 
+opt('o', 'termguicolors', true)
+opt('o', 'mouse', 'a')
+opt('o', 'shiftround', true) -- round indent to a multiple of 'shiftwidth'
 opt('o', 'undolevels', 5000)
 opt('o', 'undodir', vim.fn.expand('~/.VIM_UNDO_FILES'))
 opt('o', 'undofile', true)
-
 opt('o', 'clipboard', 'unnamedplus')
-
 opt('o', 'scrolloff', 10) -- lines of text around cursor
-
 opt('o', 'wildoptions', 'pum')
-
 opt('o', 'pumblend', 17) -- floating window popup menu for command line completion
-
 opt('o', 'ignorecase', true) -- case insensitive searching
 opt('o', 'smartcase', true) -- case-sensitive if expression contains a capital letter
-
-opt('w', 'number', true);
-opt('w', 'relativenumber', true)
+opt('o', 'updatetime', 100) -- update the swap file quicker (also helps with updating git status)
 
 opt('w', 'cursorline', true) -- highlight current line
+opt('w', 'number', true);
+opt('w', 'relativenumber', true)
+opt('w', 'foldmethod', 'syntax')
 
-opt('o', 'updatetime', 100) -- update the swap file quicker (also helps with updating git status)
