3f8989ebb40fc43dc322c41c893c9d6d519f8e4c
Greg Block <gblock0@gmail.com>
2021-03-11 14:22:44 -0800

    Update opening test file in ts

--- config/nvim/lua/test.lua
+++ config/nvim/lua/test.lua
@@ -4,11 +4,25 @@ keymap('n', '<leader>tt', ':TestNearest -strategy=neovim<CR>', { noremap = true}
 keymap('n', '<leader>tf', ':TestFile -strategy=neovim<CR>', { noremap = true})
 
 function _G.go_to_test_file(typeOfSplit)
-  local testFilePath = string.gsub(vim.fn.expand('%'), '.ts$', '.test.ts')
+  local current_path = vim.fn.expand('%')
+  if string.find(current_path, '.ts$') == nil then
+    print 'Only TS files are supported'
+    return
+  end
+  local is_test_file = string.find(current_path, '.test.ts$') ~= nil
+  local test_file_path = string.gsub(current_path, '.ts$', '.test.ts')
+  local vsplit_command = 'rightbelow vsplit '
+  local split_command = 'below split '
+  if is_test_file then
+    test_file_path = string.gsub(current_path, '.test.ts$', '.ts')
+    vsplit_command = 'vsplit '
+    split_command = 'below '
+  end
+
   if typeOfSplit == 'v' then
-    return vim.api.nvim_command(':rightbelow vsplit '..testFilePath)
+    return vim.api.nvim_command(':' .. vsplit_command ..test_file_path)
   else
-    return vim.api.nvim_command(':below split '..testFilePath)
+    return vim.api.nvim_command(':' .. split_command ..test_file_path)
   end
 
 end
