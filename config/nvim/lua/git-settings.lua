8ea3d9ff97a28cc9775971790301e93c73a855dc
Greg Block <gblock0@gmail.com>
2021-03-20 12:26:27 -0700

    Add remap for fugitive and cleanup misc remaps file

--- config/nvim/lua/git-settings.lua
+++ config/nvim/lua/git-settings.lua
@@ -1,5 +1,9 @@
+keymap = require('nutils').map
+
 require('gitsigns').setup({
   watch_index = {
     interval = 100
   },
 })
+
+keymap('n', '<leader>g', ':vertical Git<CR>')
--- config/nvim/lua/remaps.lua
+++ config/nvim/lua/remaps.lua
@@ -1,35 +1,35 @@
 -- misc remaps
-local keymap = vim.api.nvim_set_keymap
+keymap = require('nutils').map
 
 -- Close location list or quickfix window
-keymap('n', '<leader>q', ':ccl<Space><Bar><Space>lcl<CR>', {noremap = true})
+keymap('n', '<leader>q', ':ccl<Space><Bar><Space>lcl<CR>')
 
 -- Remap p so that paste doesn't overwrite the 0 register
-keymap('x', 'p', 'pgvy', { noremap = true})
+keymap('x', 'p', 'pgvy')
 
 -- Turn off highlighting, need to add the 'h' at the end of offset the <BS> making the cursor move one character to the right
-keymap('n', '<BS>', ':noh<CR> :match none<CR>h', { noremap = true})
+keymap('n', '<BS>', ':noh<CR> :match none<CR>h')
 
 -- shortcut to save
-keymap('n', '<leader><leader>', ':wa<cr>', { noremap = true})
+keymap('n', '<leader><leader>', ':wa<cr>')
 
 -- scroll the viewport faster
-keymap('n', '<C-e>', '3<C-e>', { noremap = true})
-keymap('n', '<C-y>', '3<C-y>', { noremap = true})
-keymap('n', '<leader>=', '<C-W>=', { noremap = true})
+keymap('n', '<C-e>', '3<C-e>')
+keymap('n', '<C-y>', '3<C-y>')
+keymap('n', '<leader>=', '<C-W>=')
 
 -- Reload init.vim
-keymap('n', '<leader>re', ':so ~/.config/nvim/init.vim<CR>:noh<CR>:echo "init.vim Reload!"<CR>', { noremap = true})
+keymap('n', '<leader>re', ':so ~/.config/nvim/init.vim<CR>:noh<CR>:echo "init.vim Reload!"<CR>')
 
 -- Add semi colon to the end of current line and return cursor to position
-keymap('n', '<leader>;', 'maA;<esc>`a:wa<CR>', { noremap = true})
+keymap('n', '<leader>;', 'maA;<esc>`a:wa<CR>')
 
 -- Copy current file's path to clipboard
-keymap('n', 'cp', ':let @+ = expand("%")<cr>', { noremap = true})
+keymap('n', 'cp', ':let @+ = expand("%")<cr>')
 
 -- Disable help F1 key
-keymap('', '<F1>', '<ESC>', { noremap = true})
+keymap('', '<F1>', '<ESC>')
 
 -- show/hide whitespace mappings
-keymap('n', '<F5>', ':set list!<CR>', {noremap = true})
+keymap('n', '<F5>', ':set list!<CR>')
 
