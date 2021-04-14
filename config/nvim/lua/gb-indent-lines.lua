fceaf04b4c6c84eec0d8d81d5aad0e394f08a341
Greg Block <gblock0@gmail.com>
2021-04-14 12:48:45 -0700

    Add indent-blankline setting for blank lines

--- config/nvim/lua/gb-indent-lines.lua
+++ config/nvim/lua/gb-indent-lines.lua
@@ -2,6 +2,7 @@ vim.g.indent_blankline_space_char = " "
 vim.g.indent_blankline_char = "│"
 vim.g.indent_blankline_show_current_context = true
 vim.g.indent_blankline_space_char_blankline = " "
+vim.g.indent_blankline_show_trailing_blankline_indent = false
 
 vim.g.indent_blankline_context_patterns = {
   "class",
