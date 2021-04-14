93019b6835de3d23367897c514f13b83da330fd1
Greg Block <gblock0@gmail.com>
2021-04-14 12:48:35 -0700

    Add shortcut for git blame

--- config/nvim/lua/gb-git.lua
+++ config/nvim/lua/gb-git.lua
@@ -11,3 +11,4 @@ require("gitsigns").setup(
 keymap("n", "<leader>gs", ":vertical Git<CR>", {silent = true})
 keymap("n", "<leader>gh", ":diffget //2<CR>", {silent = true})
 keymap("n", "<leader>gl", ":diffget //3<CR>", {silent = true})
+keymap("n", "<leader>gb", ":Git blame<CR>", {silent = true})
