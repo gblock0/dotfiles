5c6bfa3d07ec1d7437396175377b8574545eedd9
Greg Block <gblock0@gmail.com>
2021-04-20 11:33:05 -0700

    Update telescope bindings

--- config/nvim/lua/gb/finder.lua
+++ config/nvim/lua/gb/finder.lua
@@ -60,10 +60,11 @@ end
 keymap("n", "<leader>a", ":lua gb_grep_files()<CR>")
 keymap("n", "<leader>p", ":lua gb_find_proj_files()<Cr>")
 keymap("n", "<leader>gw", ":Telescope git_worktree git_worktrees<CR>")
+keymap("n", "<leader>ca", ":Telescope lsp_code_actions<CR>")
 
 -- Create a new vsplit, switch to it and open CtrlP
-keymap("n", "<leader>w", "<C-w>v<C-w>l :Telescope find_files<cr>")
+keymap("n", "<leader>w", "<C-w>v<C-w>l :lua gb_find_proj_files()<cr>")
 
 -- Create a new split, switch to it and open CtrlP
-keymap("n", "<leader>s", "<C-w>s<C-w>j :Telescope find_files<cr>")
+keymap("n", "<leader>s", "<C-w>s<C-w>j :lua gb_find_proj_files()<cr>")
 keymap("n", "<F3>", ":Telescope lsp_references<CR>")
