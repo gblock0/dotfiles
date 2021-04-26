f66a0573ad3a159ef584949c33412594849deb9e
Greg Block <gblock0@gmail.com>
2021-04-26 07:42:30 -0700

    Auto add <CMD> and <CR> to remaps if cmd_cr=true is passed in

--- config/nvim/lua/gb/lsp-trouble.lua
+++ config/nvim/lua/gb/lsp-trouble.lua
@@ -1,5 +1,5 @@
 local keymap = require("gb.utils").map
 
-keymap("n", "<leader>lt", "LspTroubleToggle", {silent = true})
-keymap("n", "<leader>ldt", "LspTroubleDocumentToggle", {silent = true})
-keymap("n", "<leader>lwt", "LspTroubleWorkspaceToggle", {silent = true})
+keymap("n", "<leader>lt", "LspTroubleToggle", {silent = true, cmd_cr = true})
+keymap("n", "<leader>ldt", "LspTroubleDocumentToggle", {silent = true, cmd_cr = true})
+keymap("n", "<leader>lwt", "LspTroubleWorkspaceToggle", {silent = true, cmd_cr = true})
