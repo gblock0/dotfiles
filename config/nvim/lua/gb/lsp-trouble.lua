0596c59e828278f3adad3599161252c5db750571
Greg Block <gblock0@gmail.com>
2021-07-08 12:05:42 -0500

    Add trouble diagnostics mapping

--- config/nvim/lua/gb/lsp-trouble.lua
+++ config/nvim/lua/gb/lsp-trouble.lua
@@ -1,5 +1,6 @@
 local keymap = require("gb.utils").map
 
 keymap("n", "<leader>lt", "LspTroubleToggle", {silent = true, cmd_cr = true})
+keymap("n", "<leader>ld", "Trouble lsp_document_diagnostics", {silent = true, cmd_cr = true})
 keymap("n", "<leader>ldt", "LspTroubleDocumentToggle", {silent = true, cmd_cr = true})
 keymap("n", "<leader>lwt", "LspTroubleWorkspaceToggle", {silent = true, cmd_cr = true})
