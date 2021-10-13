5bb31f0d4b8cd97a4085b6142a7dc24454df59b0
Greg Block <gblock0@gmail.com>
2021-10-13 09:21:45 -0500

    Update nvim-tree settings

--- config/nvim/lua/gb/file-explorer.lua
+++ config/nvim/lua/gb/file-explorer.lua
@@ -1,17 +1,26 @@
 local keymap = require("gb.utils").map
-vim.g.nvim_tree_width = 40 --30 by default
 vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache", ".DS_Store"} --empty by default
-vim.g.nvim_tree_auto_close = 1 --0 by default, closes the tree when it's the last window
-vim.g.nvim_tree_follow = 1 --0 by default, this option allows the cursor to be updated when entering a buffer
-vim.g.nvim_tree_group_empty = 1
 vim.g.nvim_tree_group_empty = 1
 vim.g.nvim_tree_root_folder_modifier = ":~" --This is the default. See :help filename-modifiers for more options
-vim.g.nvim_tree_lsp_diagnostics = 1 --0 by default, will show lsp diagnostics in the signcolumn. See :help nvim_tree_lsp_diagnostics
 
 keymap("n", "<leader>n", ":NvimTreeToggle<CR>", {silent = true})
 
 local tree_cb = require "nvim-tree.config".nvim_tree_callback
-vim.g.nvim_tree_bindings = {
-  {key = "w", cb = tree_cb("vsplit")},
-  {key = "s", cb = tree_cb("split")}
+require "nvim-tree".setup {
+  auto_close = false,
+  diagnostics = {
+    enable = false
+  },
+  view = {
+    width = 40,
+    mappings = {
+      list = {
+        {key = "w", cb = tree_cb("vsplit")},
+        {key = "s", cb = tree_cb("split")}
+      }
+    }
+  },
+  update_focused_file = {
+    enable = true
+  }
 }
