5a466ba85528ad33fd981f4f5dd2a72e64014f58
Greg Block <gblock0@gmail.com>
2021-12-07 09:40:48 -0600

    Update Treesitter config

--- config/nvim/lua/gb/treesitter.lua
+++ config/nvim/lua/gb/treesitter.lua
@@ -33,5 +33,23 @@ require "nvim-treesitter.configs".setup {
     enable = true,
     use_virtual_text = true,
     lint_events = {"BufWrite", "CursorHold"}
+  },
+  playground = {
+    enable = true,
+    updatetime = 25,
+    persist_queries = true,
+    keybindings = {
+      toggle_query_editor = "o",
+      toggle_hl_groups = "i",
+      toggle_injected_languages = "t",
+      -- This shows stuff like literal strings, commas, etc.
+      toggle_anonymous_nodes = "a",
+      toggle_language_display = "I",
+      focus_language = "f",
+      unfocus_language = "F",
+      update = "R",
+      goto_node = "<cr>",
+      show_help = "?"
+    }
   }
 }
