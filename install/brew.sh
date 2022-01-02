dfbf63f77df6dcc1fa5872ee07aefc002ca7b6b8
Greg Block <gblock0@gmail.com>
2022-01-02 17:02:39 -0600

    Add bottom and hyperfine to brew installs

--- install/brew.sh
+++ install/brew.sh
@@ -6,9 +6,9 @@ brew tap homebrew/cask-fonts
 
 # development tools
 brew install neovim node git reattach-to-user-namespace\
-             tmux fzf zsh antigen git-delta stow\
+             tmux fzf zsh antigen git-delta stow bottom\
              romkatv/powerlevel10k/powerlevel10k rust tree\
-             wget ripgrep procs tldr exa htop nvm
+             wget ripgrep procs tldr exa htop nvm hyperfine
 
 # Split these out because they will stop the install if they already exist
 brew install alacritty
