f4fa3c55977de0fb7ae8ca4a8a592b5178d94bb5
Greg Block <gblock0@users.noreply.github.com>
2022-12-06 17:15:25 -0600

    Add pyenv to brew install

--- install/brew.sh
+++ install/brew.sh
@@ -8,7 +8,8 @@ brew tap homebrew/cask-fonts
 brew install neovim node git reattach-to-user-namespace\
              tmux fzf zsh antigen git-delta stow bottom\
              romkatv/powerlevel10k/powerlevel10k rust tree\
-             wget ripgrep procs tldr exa htop nvm hyperfine
+             wget ripgrep procs tldr exa htop nvm hyperfine\
+             pyenv
 
 # Split these out because they will stop the install if they already exist
 brew install alacritty
