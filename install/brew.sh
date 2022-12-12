4fb66bf8a5b1544d3cf5f324be9407d5fc24d114
Greg Block <gblock0@users.noreply.github.com>
2022-12-12 11:12:29 -0600

    install gpg

--- install/brew.sh
+++ install/brew.sh
@@ -9,7 +9,7 @@ brew install neovim node git reattach-to-user-namespace\
              tmux fzf zsh antigen git-delta stow bottom\
              romkatv/powerlevel10k/powerlevel10k rust tree\
              wget ripgrep procs tldr exa htop nvm hyperfine\
-             pyenv
+             pyenv gnupg
 
 # Split these out because they will stop the install if they already exist
 brew install alacritty
