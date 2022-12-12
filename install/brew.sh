b5937a17097dd9ea18a48542f804c0d677533824
Greg Block <gblock0@users.noreply.github.com>
2022-12-12 12:10:10 -0600

    Add antigen to brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -9,8 +9,8 @@ brew install neovim node git reattach-to-user-namespace\
              tmux fzf zsh antigen git-delta stow bottom\
              romkatv/powerlevel10k/powerlevel10k rust tree\
              wget ripgrep procs tldr exa htop nvm hyperfine\
-             pyenv gnupg
-
+             pyenv gnupg antigen
+ 
 # Split these out because they will stop the install if they already exist
 brew install alacritty
 brew install font-blex-mono-nerd-font
