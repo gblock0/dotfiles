bbccc36414d403f99d13ab19904fdd337f1c2aa8
Greg Block <gblock0@users.noreply.github.com>
2023-01-14 13:05:34 -0600

    Convert to init lua (#6)
    * Move from packer to lazy.nvim for package management
    
    * Update diffview settings
    
    * update how fzf is loaded
    
    * Fix setup of lazy

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,39 @@
+-- Bootstrap lazy.nvim if it doesn't exist
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.runtimepath:prepend(lazypath)
+require('gb.plugins')
+require("gb.folds")
+require('gb.sets')
+
+-- load plugin configs
+require("gb.icons")
+require("gb.autoswap")
+require("gb.colorizer")
+require("gb.commenter")
+require("gb.debugger")
+require("gb.file-explorer")
+require("gb.finder")
+require("gb.format")
+require("gb.git")
+require("gb.harpoon")
+require("gb.hlslens")
+require("gb.lsp")
+require("gb.lsp-trouble")
+require("gb.maximizer")
+require("gb.remaps")
+require("gb.statusline")
+require("gb.term")
+require("gb.test")
+require("gb.tmux-navigator")
+require("gb.treesitter")
+require("gb.centerpad")
