4447878ea2feeec287fa45c00009317991c4dd87
Greg Block <gblock0@gmail.com>
2023-01-14 13:04:21 -0600

    Fix setup of lazy

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,3 @@
-require("gb.folds")
-require('gb.sets')
-
 -- Bootstrap lazy.nvim if it doesn't exist
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -15,3 +12,28 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.runtimepath:prepend(lazypath)
 require('gb.plugins')
+require("gb.folds")
+require('gb.sets')
+
+-- load plugin configs
+require("gb.icons")
+require("gb.autoswap")
+require("gb.colorizer")
+require("gb.commenter")
+require("gb.debugger")
+require("gb.file-explorer")
+require("gb.finder")
+require("gb.format")
+require("gb.git")
+require("gb.harpoon")
+require("gb.hlslens")
+require("gb.lsp")
+require("gb.lsp-trouble")
+require("gb.maximizer")
+require("gb.remaps")
+require("gb.statusline")
+require("gb.term")
+require("gb.test")
+require("gb.tmux-navigator")
+require("gb.treesitter")
+require("gb.centerpad")
