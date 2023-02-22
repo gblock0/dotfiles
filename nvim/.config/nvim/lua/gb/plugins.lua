178af2e7abdfa054c3558807c7dc74f2ea308d3d
Greg Block <gregory.block@tempus.com>
2023-02-22 14:30:46 -0600

    Hide up to date packages

--- nvim/.config/nvim/lua/gb/plugins.lua
+++ nvim/.config/nvim/lua/gb/plugins.lua
@@ -192,7 +192,9 @@ require('lazy').setup({
     "vuki656/package-info.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
     config = function()
-      require("package-info").setup()
+      require("package-info").setup({
+        hide_up_to_date = true
+      })
     end
   },
 
