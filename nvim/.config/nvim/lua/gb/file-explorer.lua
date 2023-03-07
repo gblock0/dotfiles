f1ec4e6c0f70001f37e38b6ce8859e2a6943d82a
Greg Block <gregory.block@tempus.com>
2023-03-07 08:38:41 -0600

    Fix nvim tree

--- nvim/.config/nvim/lua/gb/file-explorer.lua
+++ nvim/.config/nvim/lua/gb/file-explorer.lua
@@ -1,6 +1,47 @@
 vim.keymap.set("n", "<leader>n", "<cmd>NvimTreeToggle<CR>", {silent = true})
 
-local tree_cb = require "nvim-tree.config".nvim_tree_callback
+-- local tree_cb = require "nvim-tree.config".nvim_tree_callback
+local function vsplit()
+    -- open as vsplit on current node
+    local action = "vsplit"
+    local node = lib.get_node_at_cursor()
+
+    -- Just copy what's done normally with vsplit
+    if node.link_to and not node.nodes then
+        require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
+
+    elseif node.nodes ~= nil then
+        lib.expand_or_collapse(node)
+
+    else
+        require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
+
+    end
+
+    -- Finally refocus on tree if it was lost
+    view.focus()
+end
+
+local function split()
+    -- open as vsplit on current node
+    local action = "split"
+    local node = lib.get_node_at_cursor()
+
+    -- Just copy what's done normally with vsplit
+    if node.link_to and not node.nodes then
+        require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
+
+    elseif node.nodes ~= nil then
+        lib.expand_or_collapse(node)
+
+    else
+        require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
+
+    end
+
+    -- Finally refocus on tree if it was lost
+    view.focus()
+end
 require "nvim-tree".setup {
   diagnostics = {
     enable = false
@@ -9,8 +50,8 @@ require "nvim-tree".setup {
     width = 40,
     mappings = {
       list = {
-        {key = "w", cb = tree_cb("vsplit")},
-        {key = "s", cb = tree_cb("split")}
+        {key = "w", action = "vsplit", action_cb = vsplit},
+        {key = "s", action = "split", action_cb = split},
       }
     }
   },
