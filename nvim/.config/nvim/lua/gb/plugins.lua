4fce619d5a9f316108949271bd13f35a33349a75
Greg Block <gregory.block@tempus.com>
2023-04-24 13:12:17 -0500

    Add venv selector

--- nvim/.config/nvim/lua/gb/plugins.lua
+++ nvim/.config/nvim/lua/gb/plugins.lua
@@ -217,4 +217,10 @@ require('lazy').setup({
   },
   { "williamboman/mason.nvim", build = ":MasonUpdate" },
   "williamboman/mason-lspconfig.nvim",
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
+    config = true,
+    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  }
 })
