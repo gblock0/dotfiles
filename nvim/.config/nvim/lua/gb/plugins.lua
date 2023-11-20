216dc2924cebd1ad3eec3549210ef5831dadde4b
Greg Block <gregory.block@tempus.com>
2023-11-20 17:53:36 -0600

    Delete old plugins that I don't use anymore (dab and bqf)

--- nvim/.config/nvim/lua/gb/plugins.lua
+++
@@ -1,8 +0,0 @@
-require('lazy').setup({
-  -- Adds debugger to nvim
-  -- "puremourning/vimspector",
-  "mfussenegger/nvim-dap",
-  "rcarriga/nvim-dap-ui",
-  "nvim-telescope/telescope-dap.nvim",
-  "theHamsta/nvim-dap-virtual-text",
-})
