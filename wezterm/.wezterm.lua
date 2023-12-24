4eda24125913d9ae298a177967b5e4a6e68319bb
Greg Block <gblock0@gmail.com>
2023-12-23 18:50:42 -0600

    add some more keymaps for wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -14,9 +14,36 @@ config.font = wezterm.font('BlexMono Nerd Font Mono')
 config.font_size = 18.0
 
 -- For example, changing the color scheme:
-config.color_scheme = 'MaterialDark'
+config.color_scheme = 'arcoiris'
 
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }
+
+local move_around = function(window, pane, direction_wez, direction_nvim)
+  if pane:get_title():sub(1, 4) == "nvim" then
+    window:perform_action(wezterm.action { SendString = "\x17" .. direction_nvim }, pane)
+  else
+    window:perform_action(wezterm.action { ActivatePaneDirection = direction_wez }, pane)
+  end
+end
+
+wezterm.on("move-left", function(window, pane)
+  move_around(window, pane, "Left", "h")
+end)
+
+wezterm.on("move-right", function(window, pane)
+  move_around(window, pane, "Right", "l")
+end)
+
+wezterm.on("move-up", function(window, pane)
+  move_around(window, pane, "Up", "k")
+end)
+
+wezterm.on("move-down", function(window, pane)
+  move_around(window, pane, "Down", "j")
+end)
+
+config.disable_default_key_bindings = true
+
 config.keys = {
   {
     key = '|',
@@ -28,12 +55,26 @@ config.keys = {
     mods = 'LEADER',
     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
   },
+  {
+    key = 'x',
+    mods = 'LEADER',
+    action = wezterm.action.CloseCurrentPane { confirm = false },
+  },
+  {
+    key = 'z',
+    mods = 'LEADER',
+    action = wezterm.action.TogglePaneZoomState
+  },
   -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
   {
     key = 'a',
     mods = 'LEADER|CTRL',
     action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' },
   },
+  { key = "LeftArrow",  mods = "SHIFT", action = wezterm.action { EmitEvent = "move-left" } },
+  { key = "RightArrow", mods = "SHIFT", action = wezterm.action { EmitEvent = "move-right" } },
+  { key = "UpArrow",    mods = "SHIFT", action = wezterm.action { EmitEvent = "move-up" } },
+  { key = "j",  mods = "LEADER|CTRL", action = wezterm.action { EmitEvent = "move-down" } },
 }
 
 -- LEADER + CTRL + number to activate that tab
@@ -44,7 +85,6 @@ for i = 1, 8 do
     action = wezterm.action.ActivateTab(i - 1),
   })
 end
-
 
 -- and finally, return the configuration to wezterm
 return config
