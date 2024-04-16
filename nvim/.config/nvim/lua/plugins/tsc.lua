ce9e8891574e9f4be7e5061d3102eea70e25f665
Greg Block <gregory.block@tempus.com>
2024-04-16 08:31:47 -0500

    Use trouble instead of qflist for tsc and auto close when all errors are resolved

--- nvim/.config/nvim/lua/plugins/tsc.lua
+++ nvim/.config/nvim/lua/plugins/tsc.lua
@@ -2,6 +2,8 @@ return {
   'dmmulroy/tsc.nvim',
   opts = {
     auto_start_watch_mode = true,
+    use_trouble_qflist = true,
+    auto_close_qflist = true,
     flags = {
       watch = true,
     },
