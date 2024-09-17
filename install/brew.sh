829e1a6fbea0bd746b3d492898052076339d3463
Greg Block <gblock0@gmail.com>
2024-09-17 13:20:52 -0500

    Add direnv

--- install/brew.sh
+++ install/brew.sh
@@ -15,7 +15,7 @@ brew install neovim node git reattach-to-user-namespace\
              tmux fzf zsh antigen git-delta stow bottom\
              romkatv/powerlevel10k/powerlevel10k rust tree\
              wget ripgrep procs tldr eza htop nvm hyperfine\
-             gnupg cmake yarn mise bat
+             gnupg cmake yarn mise bat direnv
 
 brew install --cask hammerspoon
 
